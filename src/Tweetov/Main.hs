e9fd89612d6487f45b3e4d0627eb5660c79a343f
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-06-12 11:03:47 +0200

    Configurable listen address & port

--- src/Tweetov/Main.hs
+++ src/Tweetov/Main.hs
@@ -3,22 +3,29 @@ module Main
     ( main
     ) where
 
-import Snap.Types (Snap)
 import Snap.Http.Server (httpServe) 
-import Snap.Http.Server.Config ( Config, defaultConfig
+import Snap.Http.Server.Config ( ConfigListen (..)
+                               , emptyConfig, addListen
                                , setAccessLog, setErrorLog
                                )
+import System.Environment (getArgs, getProgName)
+import qualified Data.ByteString.Char8 as B
 
 import Tweetov.Application
 
 -- | Main function
 --
 main :: IO ()
-main = httpServe config application
-
--- | Site config
---
-config :: Config Snap a
-config = setAccessLog Nothing
-       $ setErrorLog Nothing
-       $ defaultConfig
+main = do
+    args <- getArgs
+    progName <- getProgName
+    case args of
+        [address, port] -> httpServe (config address port) application
+        _               -> putStrLn $
+            "Usage: " ++ progName ++ " <listen address> <port>"
+  where
+    config address port =
+        addListen (ListenHttp (B.pack address) (read port)) $
+        setAccessLog Nothing $
+        setErrorLog Nothing $
+        emptyConfig
